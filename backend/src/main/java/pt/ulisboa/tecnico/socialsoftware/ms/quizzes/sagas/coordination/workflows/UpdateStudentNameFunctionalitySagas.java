e8a05fbb8b6834cef790a4c796ab0b37e680e13f
André Esgalhado <andre.esgalhado@tecnico.ulisboa.pt>
2024-10-10 14:44:36 +0100

    removed unecessary parts of updateStudentName

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/ms/quizzes/sagas/coordination/workflows/UpdateStudentNameFunctionalitySagas.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/ms/quizzes/sagas/coordination/workflows/UpdateStudentNameFunctionalitySagas.java
@@ -3,14 +3,10 @@ package pt.ulisboa.tecnico.socialsoftware.ms.quizzes.sagas.coordination.workflow
 import static pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.exception.ErrorMessage.USER_MISSING_NAME;
 
 import pt.ulisboa.tecnico.socialsoftware.ms.coordination.workflow.WorkflowFunctionality;
-import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.exception.ErrorMessage;
 import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.exception.TutorException;
 import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.execution.aggregate.CourseExecutionFactory;
 import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.execution.service.CourseExecutionService;
 import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.user.aggregate.UserDto;
-import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.sagas.aggregates.dtos.SagaCourseExecutionDto;
-import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.sagas.aggregates.states.CourseExecutionSagaState;
-import pt.ulisboa.tecnico.socialsoftware.ms.sagas.aggregate.GenericSagaState;
 import pt.ulisboa.tecnico.socialsoftware.ms.sagas.unityOfWork.SagaUnitOfWork;
 import pt.ulisboa.tecnico.socialsoftware.ms.sagas.unityOfWork.SagaUnitOfWorkService;
 import pt.ulisboa.tecnico.socialsoftware.ms.sagas.workflow.SagaSyncStep;
@@ -37,25 +33,8 @@ public class UpdateStudentNameFunctionalitySagas extends WorkflowFunctionality {
         }
 
         SagaSyncStep updateStudentNameStep = new SagaSyncStep("updateStudentNameStep", () -> {
-            SagaCourseExecutionDto courseExecution = (SagaCourseExecutionDto) courseExecutionService.getCourseExecutionById(executionAggregateId, unitOfWork);
-            if (courseExecution.getSagaState().equals(GenericSagaState.NOT_IN_SAGA)) {
-                unitOfWorkService.registerSagaState(executionAggregateId, CourseExecutionSagaState.IN_UPDATE_NAME, unitOfWork);
-            }
-            else {
-                throw new TutorException(ErrorMessage.AGGREGATE_BEING_USED_IN_OTHER_SAGA);
-            }
-
-            UserDto student = courseExecution.getStudents().stream().filter(s -> s.getAggregateId().equals(userAggregateId)).findFirst().orElse(null);
-            if (student == null) {
-                throw new TutorException(ErrorMessage.COURSE_EXECUTION_STUDENT_NOT_FOUND, userAggregateId, executionAggregateId);
-            }
-
             courseExecutionService.updateExecutionStudentName(executionAggregateId, userAggregateId, userDto.getName(), unitOfWork);
         });
-
-        updateStudentNameStep.registerCompensation(() -> {
-            unitOfWorkService.registerSagaState(executionAggregateId, GenericSagaState.NOT_IN_SAGA, unitOfWork);
-        }, unitOfWork);
     
         workflow.addStep(updateStudentNameStep);
     }
