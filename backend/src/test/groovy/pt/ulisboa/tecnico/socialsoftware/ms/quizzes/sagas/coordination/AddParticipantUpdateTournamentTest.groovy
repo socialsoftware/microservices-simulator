6078144682c1b71a6f9e788fb9e5381d5fcae4da
André Esgalhado <andre.esgalhado@tecnico.ulisboa.pt>
2024-10-22 10:56:45 +0100

    AddParticipantUpdateTournamentTest sagas

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/ms/quizzes/sagas/coordination/AddParticipantUpdateTournamentTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/ms/quizzes/sagas/coordination/AddParticipantUpdateTournamentTest.groovy
@@ -8,17 +8,33 @@ import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.QuizzesSpockTest
 import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.coordination.functionalities.CourseExecutionFunctionalities
 import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.coordination.functionalities.QuizFunctionalities
 import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.coordination.functionalities.TournamentFunctionalities
+import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.tournament.events.handling.TournamentEventHandling
+import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.exception.ErrorMessage
+import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.exception.TutorException
 import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.execution.aggregate.CourseExecutionDto
 import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.question.aggregate.QuestionDto
 import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.topic.aggregate.TopicDto
 import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.tournament.aggregate.TournamentDto
 import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.user.aggregate.UserDto
+import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.tournament.service.TournamentService
+import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.topic.service.TopicService
+import pt.ulisboa.tecnico.socialsoftware.ms.quizzes.microservices.quiz.service.QuizService
 import pt.ulisboa.tecnico.socialsoftware.ms.sagas.unityOfWork.SagaUnitOfWorkService
 
 @DataJpaTest
 class AddParticipantUpdateTournamentTest extends QuizzesSpockTest {
     @Autowired
     private SagaUnitOfWorkService unitOfWorkService
+
+    @Autowired
+    private TournamentService tournamentService
+    @Autowired
+    private TopicService topicService
+    @Autowired
+    private QuizService quizService
+
+    @Autowired
+    private TournamentEventHandling tournamentEventHandling
 
     @Autowired
     private CourseExecutionFunctionalities courseExecutionFunctionalities
@@ -32,6 +48,8 @@ class AddParticipantUpdateTournamentTest extends QuizzesSpockTest {
     private TopicDto topicDto1, topicDto2, topicDto3
     private QuestionDto questionDto1, questionDto2, questionDto3
     private TournamentDto tournamentDto
+
+    def unitOfWork1, unitOfWork2
 
     def setup() {
         given: 'a course execution'
@@ -57,6 +75,11 @@ class AddParticipantUpdateTournamentTest extends QuizzesSpockTest {
 
         and: 'a tournament where the first user is the creator'
         tournamentDto = createTournament(TIME_1, TIME_3, 2, userCreatorDto.getAggregateId(),  courseExecutionDto.getAggregateId(), [topicDto1.getAggregateId(),topicDto2.getAggregateId()])
+    
+        def functionalityName1 = UpdateTournamentFunctionalitySagas.class.getSimpleName()
+        def functionalityName2 = AddParticipantFunctionalitySagas.class.getSimpleName()
+        unitOfWork1 = unitOfWorkService.createUnitOfWork(functionalityName1)
+        unitOfWork2 = unitOfWorkService.createUnitOfWork(functionalityName2)
     }
 
     def cleanup() {
@@ -64,7 +87,64 @@ class AddParticipantUpdateTournamentTest extends QuizzesSpockTest {
     }
 
     def "add participant find the tournament in update and aborts"() {
+        given: 'the tournament is updating'
+        def topicsAggregateIds = [topicDto1.getAggregateId(), topicDto2.getAggregateId(), topicDto3.getAggregateId()].toSet()
+        def updateTournamentFunctionality = new UpdateTournamentFunctionalitySagas(tournamentService, topicService, quizService, unitOfWorkService, tournamentDto, topicsAggregateIds, unitOfWork1)
+        
+        updateTournamentFunctionality.executeUntilStep("getOriginalTournamentStep", unitOfWork1)
 
+        when: 'a participant is added'
+        tournamentFunctionalities.addParticipant(tournamentDto.getAggregateId(), courseExecutionDto.getAggregateId(), userCreatorDto.getAggregateId())
+
+        then: 'fails because the tournament is being updated'
+        def error = thrown(TutorException)
+        error.errorMessage == ErrorMessage.AGGREGATE_BEING_USED_IN_OTHER_SAGA
+
+        and: 'no participant is added to the tournament'
+        def tournamentDtoResult = tournamentFunctionalities.findTournament(tournamentDto.getAggregateId())
+        tournamentDtoResult.getParticipants().size() == 0
+    }
+
+    def "add participant then update tournament"() {
+        given: 'topics to update the tournament'
+        tournamentDto.setNumberOfQuestions(3)
+        def topicsAggregateIds = [topicDto1.getAggregateId(), topicDto2.getAggregateId(), topicDto3.getAggregateId()].toSet()
+        
+        when: 'a participant is added'
+        tournamentFunctionalities.addParticipant(tournamentDto.getAggregateId(), courseExecutionDto.getAggregateId(), userCreatorDto.getAggregateId())
+
+        and: 'the tournament is updated'
+        tournamentFunctionalities.updateTournament(tournamentDto, topicsAggregateIds)
+
+        then: 'the participant is added'
+        def tournamentDtoResult = tournamentFunctionalities.findTournament(tournamentDto.getAggregateId())
+        tournamentDtoResult.getParticipants().size() == 1
+        and: 'the tournament is updated'
+        tournamentDtoResult.numberOfQuestions == 3
+        tournamentDtoResult.topics*.aggregateId.toSet() == [topicDto1.getAggregateId(), topicDto2.getAggregateId(), topicDto3.getAggregateId()].toSet()
+        def quizDto = quizFunctionalities.findQuiz(tournamentDtoResult.quiz.aggregateId)
+        quizDto.questionDtos.size() == 3
+    }
+
+    def "update tournament then add participant"() {
+        given: 'topics to update the tournament'
+        tournamentDto.setNumberOfQuestions(3)
+        def topicsAggregateIds = [topicDto1.getAggregateId(), topicDto2.getAggregateId(), topicDto3.getAggregateId()].toSet()
+        
+        when: 'the tournament is updated'
+        tournamentFunctionalities.updateTournament(tournamentDto, topicsAggregateIds)
+
+        and: 'a participant is added'
+        tournamentFunctionalities.addParticipant(tournamentDto.getAggregateId(), courseExecutionDto.getAggregateId(), userCreatorDto.getAggregateId())
+
+        then: 'the participant is added'
+        def tournamentDtoResult = tournamentFunctionalities.findTournament(tournamentDto.getAggregateId())
+        tournamentDtoResult.getParticipants().size() == 1
+        and: 'the tournament is updated'
+        tournamentDtoResult.numberOfQuestions == 3
+        tournamentDtoResult.topics*.aggregateId.toSet() == [topicDto1.getAggregateId(), topicDto2.getAggregateId(), topicDto3.getAggregateId()].toSet()
+        def quizDto = quizFunctionalities.findQuiz(tournamentDtoResult.quiz.aggregateId)
+        quizDto.questionDtos.size() == 3
     }
 
 
