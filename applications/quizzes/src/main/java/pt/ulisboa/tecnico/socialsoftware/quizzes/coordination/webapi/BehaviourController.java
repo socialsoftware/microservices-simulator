ceefd4937058abd84057d39c7da5abd716fb816f
Tomás Nascimento <gomes.nascimento@tecnico.ulisboa.pt>
2025-04-16 16:15:35 +0100

    Update BehaviourController

--- applications/quizzes/src/main/java/pt/ulisboa/tecnico/socialsoftware/quizzes/coordination/webapi/BehaviourController.java
+++ applications/quizzes/src/main/java/pt/ulisboa/tecnico/socialsoftware/quizzes/coordination/webapi/BehaviourController.java
@@ -4,6 +4,9 @@ import java.io.File;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import pt.ulisboa.tecnico.socialsoftware.ms.utils.ExecutionParametersService;
@@ -15,11 +18,19 @@ public class BehaviourController {
     @Autowired
     private ExecutionParametersService executionParametersService;
 
-    @GetMapping(value = "/behaviour/load")
-    public String load() {
+   @PostMapping("/behaviour/load/{dir}")
+    public String load(@PathVariable String dir) {
         System.out.println("Behaviour load started");
-        //executionParametersService.LoadDir(mavenBaseDir);
-        System.out.println("Maven base dir: " + mavenBaseDir);
+        executionParametersService.LoadDir(mavenBaseDir, dir);
+        System.out.println("Provided dir: " + dir);
+        return "OK";
+    }
+
+    
+    @GetMapping(value = "/behaviour/clean")
+    public String clean() {
+        System.out.println("Report clean started");
+        executionParametersService.cleanReportFile();
         return "OK";
     }
 }
