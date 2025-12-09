d4e1e1f3c829034988cbd6287a2fb297dc793819
Daniel Pereira <danielppereira2002@gmail.com>
2025-12-09 17:02:35 +0000

    Add EventSubscriberService to aggregates that do not subscribe to any events

---
+++ applications/quizzes/src/main/java/pt/ulisboa/tecnico/socialsoftware/quizzes/microservices/topic/events/TopicEventSubscriberService.java
@@ -0,0 +1,32 @@
+package pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.topic.events;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.messaging.Message;
+import java.util.function.Consumer;
+import java.util.Map;
+import java.util.Collections;
+import pt.ulisboa.tecnico.socialsoftware.ms.domain.event.Event;
+import pt.ulisboa.tecnico.socialsoftware.ms.domain.event.EventRepository;
+import pt.ulisboa.tecnico.socialsoftware.ms.domain.event.EventSubscriberService;
+import pt.ulisboa.tecnico.socialsoftware.ms.coordination.workflow.stream.MessagingObjectMapperProvider;
+
+@Component
+@Profile("stream")
+public class TopicEventSubscriberService extends EventSubscriberService {
+
+    public TopicEventSubscriberService(EventRepository eventRepository, MessagingObjectMapperProvider mapperProvider) {
+        super(eventRepository, mapperProvider);
+    }
+
+    @Override
+    public Map<String, Class<? extends Event>> getSubscribedEvents() {
+        return Collections.emptyMap();
+    }
+
+    @Bean
+    public Consumer<Message<String>> topicEventSubscriber() {
+        return this::processEvent;
+    }
+}
---
+++ applications/quizzes/src/main/java/pt/ulisboa/tecnico/socialsoftware/quizzes/microservices/user/events/UserEventSubscriberService.java
@@ -0,0 +1,32 @@
+package pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.user.events;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.messaging.Message;
+import java.util.function.Consumer;
+import java.util.Map;
+import java.util.Collections;
+import pt.ulisboa.tecnico.socialsoftware.ms.domain.event.Event;
+import pt.ulisboa.tecnico.socialsoftware.ms.domain.event.EventRepository;
+import pt.ulisboa.tecnico.socialsoftware.ms.domain.event.EventSubscriberService;
+import pt.ulisboa.tecnico.socialsoftware.ms.coordination.workflow.stream.MessagingObjectMapperProvider;
+
+@Component
+@Profile("stream")
+public class UserEventSubscriberService extends EventSubscriberService {
+
+    public UserEventSubscriberService(EventRepository eventRepository, MessagingObjectMapperProvider mapperProvider) {
+        super(eventRepository, mapperProvider);
+    }
+
+    @Override
+    public Map<String, Class<? extends Event>> getSubscribedEvents() {
+        return Collections.emptyMap();
+    }
+
+    @Bean
+    public Consumer<Message<String>> userEventSubscriber() {
+        return this::processEvent;
+    }
+}
