9414f6287b5c7a90059d6db8ecc8e533f71c3181
Daniel Pereira <danielppereira2002@gmail.com>
2025-12-10 11:08:10 +0000

    AnswerService application

---
+++ applications/quizzes/src/main/java/pt/ulisboa/tecnico/socialsoftware/quizzes/microservices/answer/AnswerServiceApplication.java
@@ -0,0 +1,31 @@
+package pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.answer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Profile("answer-service")
+@SpringBootApplication(scanBasePackages = {
+        "pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.answer",
+        "pt.ulisboa.tecnico.socialsoftware.ms",
+})
+@EnableJpaRepositories(basePackages = {
+        "pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.answer",
+        "pt.ulisboa.tecnico.socialsoftware.ms.domain.event",
+        "pt.ulisboa.tecnico.socialsoftware.ms.domain.aggregate",
+        "pt.ulisboa.tecnico.socialsoftware.ms.sagas.aggregate",
+        "pt.ulisboa.tecnico.socialsoftware.ms.causal.aggregate",
+})
+@EntityScan(basePackages = {
+        "pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.answer",
+        "pt.ulisboa.tecnico.socialsoftware.ms"
+})
+@PropertySource({ "classpath:application-answer-service.yaml" })
+public class AnswerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnswerServiceApplication.class, args);
+    }
+}
