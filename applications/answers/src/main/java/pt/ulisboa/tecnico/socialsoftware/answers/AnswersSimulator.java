e344adf530d3dd62efedaf266c8eab5f99b8c68a
Lourenço Ponces Duarte <lourenco.poncesduarte@gmail.com>
2026-02-09 19:31:17 +0000

    Refactor AnswersSimulator configuration to improve package scanning and add scheduling and retry capabilities; replace YAML and properties files with a unified properties file for better management

--- applications/answers/src/main/java/pt/ulisboa/tecnico/socialsoftware/answers/AnswersSimulator.java
+++ applications/answers/src/main/java/pt/ulisboa/tecnico/socialsoftware/answers/AnswersSimulator.java
@@ -8,30 +8,40 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import pt.ulisboa.tecnico.socialsoftware.ms.domain.event.EventService;
 
 @PropertySource({"classpath:application.properties"})
-@EnableJpaRepositories(basePackages = {".ms.*",
-".answers.*"})
-@EntityScan(basePackages = {".ms.*",
-".answers.*"})
+@EnableJpaRepositories(basePackages = {
+    "pt.ulisboa.tecnico.socialsoftware.ms",
+    "pt.ulisboa.tecnico.socialsoftware.answers"
+})
+@EntityScan(basePackages = {
+    "pt.ulisboa.tecnico.socialsoftware.ms",
+    "pt.ulisboa.tecnico.socialsoftware.answers"
+})
 @EnableTransactionManagement
 @EnableJpaAuditing
-
-
-@SpringBootApplication(scanBasePackages = {".ms.*",
-".answers.*"})
+@EnableScheduling
+@EnableRetry
+@SpringBootApplication(scanBasePackages = {
+    "pt.ulisboa.tecnico.socialsoftware.ms",
+    "pt.ulisboa.tecnico.socialsoftware.answers"
+})
 public class AnswersSimulator implements InitializingBean {
 
+    @Autowired
+    private EventService eventService;
 
-public static void main(String[] args) {
-SpringApplication.run(AnswersSimulator.class, args);
-}
+    public static void main(String[] args) {
+        SpringApplication.run(AnswersSimulator.class, args);
+    }
 
-@Override
-public void afterPropertiesSet() {
-// Run on startup
-
-}
+    @Override
+    public void afterPropertiesSet() {
+        // Clear any stale events from previous runs
+        eventService.clearEventsAtApplicationStartUp();
+    }
 }
