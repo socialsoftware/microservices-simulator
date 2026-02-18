33daac440ed5571c6aea7003f5fda59107d050f6
Francisco Leitão <kikoeiraleitao@gmail.com>
2026-02-18 19:33:52 +0000

    test(CourseExecution): Add compensating action to CreateCourseExecution saga and add corresponding tests

--- applications/quizzes/src/main/java/pt/ulisboa/tecnico/socialsoftware/quizzes/microservices/course/commandHandler/CourseCommandHandler.java
+++ applications/quizzes/src/main/java/pt/ulisboa/tecnico/socialsoftware/quizzes/microservices/course/commandHandler/CourseCommandHandler.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pt.ulisboa.tecnico.socialsoftware.ms.coordination.workflow.command.Command;
 import pt.ulisboa.tecnico.socialsoftware.ms.coordination.workflow.command.CommandHandler;
+import pt.ulisboa.tecnico.socialsoftware.quizzes.command.course.CreateCourseRemoteCommand;
+import pt.ulisboa.tecnico.socialsoftware.quizzes.command.course.DeleteCourseCommand;
 import pt.ulisboa.tecnico.socialsoftware.quizzes.command.course.GetAndOrCreateCourseRemoteCommand;
 import pt.ulisboa.tecnico.socialsoftware.quizzes.command.course.GetCourseByIdCommand;
+import pt.ulisboa.tecnico.socialsoftware.quizzes.command.course.GetCourseByNameRemoteCommand;
 import pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.course.service.CourseService;
 
 import java.util.logging.Logger;
@@ -27,6 +30,9 @@ public class CourseCommandHandler extends CommandHandler {
         return switch (command) {
             case GetCourseByIdCommand cmd -> handleGetCourseById(cmd);
             case GetAndOrCreateCourseRemoteCommand cmd -> handleGetAndOrCreateCourseRemote(cmd);
+            case GetCourseByNameRemoteCommand cmd -> handleGetCourseByNameRemote(cmd);
+            case CreateCourseRemoteCommand cmd -> handleCreateCourseRemote(cmd);
+            case DeleteCourseCommand cmd -> handleDeleteCourse(cmd);
             default -> {
                 logger.warning("Unknown command type: " + command.getClass().getName());
                 yield null;
@@ -39,8 +45,40 @@ public class CourseCommandHandler extends CommandHandler {
         return courseService.getCourseById(command.getAggregateId(), command.getUnitOfWork());
     }
 
+    // TODO: cleanup
     private Object handleGetAndOrCreateCourseRemote(GetAndOrCreateCourseRemoteCommand command) {
         logger.info("Getting or creating course by ID: " + command.getCourseExecutionDto().getAggregateId());
         return courseService.getAndOrCreateCourseRemote(command.getCourseExecutionDto(), command.getUnitOfWork());
+    }
+
+    private Object handleGetCourseByNameRemote(GetCourseByNameRemoteCommand command) {
+        logger.info("Getting course by name: " + command.getCourseExecutionDto().getName());
+        try {
+            return courseService.getCourseByNameRemote(command.getCourseExecutionDto(), command.getUnitOfWork());
+        } catch (Exception e) {
+            logger.severe("Failed to get course by name: " + e.getMessage());
+            return e;
+        }
+    }
+
+    private Object handleCreateCourseRemote(CreateCourseRemoteCommand command) {
+        logger.info("Creating course: " + command.getCourseExecutionDto().getName());
+        try {
+            return courseService.createCourseRemote(command.getCourseExecutionDto(), command.getUnitOfWork());
+        } catch (Exception e) {
+            logger.severe("Failed to create course: " + e.getMessage());
+            return e;
+        }
+    }
+
+    private Object handleDeleteCourse(DeleteCourseCommand command) {
+        logger.info("Deleting course: " + command.getCourseAggregateId());
+        try {
+            courseService.deleteCourse(command.getCourseAggregateId(), command.getUnitOfWork());
+            return null;
+        } catch (Exception e) {
+            logger.severe("Failed to delete course: " + e.getMessage());
+            return e;
+        }
     }
 }
--- applications/quizzes/src/main/java/pt/ulisboa/tecnico/socialsoftware/quizzes/microservices/course/service/CourseService.java
+++ applications/quizzes/src/main/java/pt/ulisboa/tecnico/socialsoftware/quizzes/microservices/course/service/CourseService.java
@@ -1,58 +1,92 @@
-    package pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.course.service;
+package pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.course.service;
 
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.stereotype.Service;
-    import org.springframework.transaction.annotation.Isolation;
-    import org.springframework.transaction.annotation.Transactional;
-    import pt.ulisboa.tecnico.socialsoftware.ms.coordination.unitOfWork.UnitOfWork;
-    import pt.ulisboa.tecnico.socialsoftware.ms.coordination.unitOfWork.UnitOfWorkService;
-    import pt.ulisboa.tecnico.socialsoftware.ms.domain.aggregate.AggregateIdGeneratorService;
-    import pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.course.aggregate.Course;
-    import pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.course.aggregate.CourseCustomRepository;
-    import pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.course.aggregate.CourseDto;
-    import pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.course.aggregate.CourseFactory;
-    import pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.execution.aggregate.CourseExecutionDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+import pt.ulisboa.tecnico.socialsoftware.ms.coordination.unitOfWork.UnitOfWork;
+import pt.ulisboa.tecnico.socialsoftware.ms.coordination.unitOfWork.UnitOfWorkService;
+import pt.ulisboa.tecnico.socialsoftware.ms.domain.aggregate.AggregateIdGeneratorService;
+import pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.course.aggregate.Course;
+import pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.course.aggregate.CourseCustomRepository;
+import pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.course.aggregate.CourseDto;
+import pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.course.aggregate.CourseFactory;
+import pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.execution.aggregate.CourseExecutionDto;
 
-    @Service
-    public class CourseService {
-        @Autowired
-        private AggregateIdGeneratorService aggregateIdGeneratorService;
+@Service
+public class CourseService {
+    @Autowired
+    private AggregateIdGeneratorService aggregateIdGeneratorService;
 
-        private final UnitOfWorkService<UnitOfWork> unitOfWorkService;
+    private final UnitOfWorkService<UnitOfWork> unitOfWorkService;
 
-        private final CourseCustomRepository courseRepository;
+    private final CourseCustomRepository courseRepository;
 
-        @Autowired
-        private CourseFactory courseFactory;
+    @Autowired
+    private CourseFactory courseFactory;
 
-        public CourseService(UnitOfWorkService unitOfWorkService, CourseCustomRepository courseRepository) {
-            this.unitOfWorkService = unitOfWorkService;
-            this.courseRepository = courseRepository;
+    public CourseService(UnitOfWorkService unitOfWorkService, CourseCustomRepository courseRepository) {
+        this.unitOfWorkService = unitOfWorkService;
+        this.courseRepository = courseRepository;
+    }
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public CourseDto getCourseById(Integer aggregateId, UnitOfWork unitOfWorkWorkService) {
+        return courseFactory.createCourseDto(
+                (Course) unitOfWorkService.aggregateLoadAndRegisterRead(aggregateId, unitOfWorkWorkService));
+    }
+
+    // TODO: remove this method and its branches
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public CourseExecutionDto getAndOrCreateCourseRemote(CourseExecutionDto courseExecutionDto, UnitOfWork unitOfWork) {
+        Course course = getCourseByName(courseExecutionDto.getName(), unitOfWork);
+        if (course == null) {
+            Integer aggregateId = aggregateIdGeneratorService.getNewAggregateId();
+            course = courseFactory.createCourse(aggregateId, courseExecutionDto);
+            unitOfWorkService.registerChanged(course, unitOfWork);
         }
+        courseExecutionDto.setCourseAggregateId(course.getAggregateId());
+        courseExecutionDto.setName(course.getName());
+        courseExecutionDto.setType(course.getType().toString());
+        courseExecutionDto.setCourseVersion(course.getVersion());
+        return courseExecutionDto;
+    }
 
-        @Transactional(isolation = Isolation.SERIALIZABLE)
-        public CourseDto getCourseById(Integer aggregateId, UnitOfWork unitOfWorkWorkService) {
-            return courseFactory.createCourseDto((Course) unitOfWorkService.aggregateLoadAndRegisterRead(aggregateId, unitOfWorkWorkService));
-        }
-
-        @Transactional(isolation = Isolation.SERIALIZABLE)
-        public CourseExecutionDto getAndOrCreateCourseRemote(CourseExecutionDto courseExecutionDto, UnitOfWork unitOfWork) {
-            Course course = getCourseByName(courseExecutionDto.getName(), unitOfWork);
-            if (course == null) {
-                Integer aggregateId = aggregateIdGeneratorService.getNewAggregateId();
-                course = courseFactory.createCourse(aggregateId, courseExecutionDto);
-                unitOfWorkService.registerChanged(course, unitOfWork);
-            }
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public CourseExecutionDto getCourseByNameRemote(CourseExecutionDto courseExecutionDto, UnitOfWork unitOfWork) {
+        Course course = getCourseByName(courseExecutionDto.getName(), unitOfWork);
+        if (course != null) {
             courseExecutionDto.setCourseAggregateId(course.getAggregateId());
             courseExecutionDto.setName(course.getName());
             courseExecutionDto.setType(course.getType().toString());
             courseExecutionDto.setCourseVersion(course.getVersion());
-            return courseExecutionDto;
         }
-
-        private Course getCourseByName(String courseName, UnitOfWork unitOfWork) {
-            return courseRepository.findCourseIdByName(courseName)
-                    .map(id -> (Course) unitOfWorkService.aggregateLoadAndRegisterRead(id, unitOfWork))
-                    .orElse(null);
-        }
+        // courseAggregateId remains null if course not found
+        return courseExecutionDto;
     }
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public CourseExecutionDto createCourseRemote(CourseExecutionDto courseExecutionDto, UnitOfWork unitOfWork) {
+        Integer aggregateId = aggregateIdGeneratorService.getNewAggregateId();
+        Course course = courseFactory.createCourse(aggregateId, courseExecutionDto);
+        unitOfWorkService.registerChanged(course, unitOfWork);
+        courseExecutionDto.setCourseAggregateId(course.getAggregateId());
+        courseExecutionDto.setName(course.getName());
+        courseExecutionDto.setType(course.getType().toString());
+        courseExecutionDto.setCourseVersion(course.getVersion());
+        return courseExecutionDto;
+    }
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public void deleteCourse(Integer courseAggregateId, UnitOfWork unitOfWork) {
+        Course course = (Course) unitOfWorkService.aggregateLoadAndRegisterRead(courseAggregateId, unitOfWork);
+        course.remove();
+        unitOfWorkService.registerChanged(course, unitOfWork);
+    }
+
+    private Course getCourseByName(String courseName, UnitOfWork unitOfWork) {
+        return courseRepository.findCourseIdByName(courseName)
+                .map(id -> (Course) unitOfWorkService.aggregateLoadAndRegisterRead(id, unitOfWork))
+                .orElse(null);
+    }
+}
