c94f7d0bceef4d0125621d871131e5a70f3c4673
Francisco Leitão <kikoeiraleitao@gmail.com>
2026-02-26 16:11:28 +0000

    test(Execution): add enrollStudent tests

---
+++ applications/quizzes/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/quizzes/sagas/behaviour/execution/AddStudentEnrollFaultTest.groovy
@@ -0,0 +1,46 @@
+package pt.ulisboa.tecnico.socialsoftware.quizzes.sagas.behaviour.execution
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import pt.ulisboa.tecnico.socialsoftware.ms.exception.SimulatorException
+import pt.ulisboa.tecnico.socialsoftware.quizzes.BeanConfigurationSagas
+import pt.ulisboa.tecnico.socialsoftware.quizzes.QuizzesSpockTest
+import pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.execution.aggregate.CourseExecutionDto
+import pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.execution.coordination.functionalities.CourseExecutionFunctionalities
+import pt.ulisboa.tecnico.socialsoftware.quizzes.microservices.user.aggregate.UserDto
+
+@DataJpaTest
+class AddStudentEnrollFaultTest extends QuizzesSpockTest {
+    @Autowired
+    private CourseExecutionFunctionalities courseExecutionFunctionalities
+
+    private CourseExecutionDto courseExecutionDto
+    private UserDto userDto
+
+    def setup() {
+        loadBehaviorScripts()
+        courseExecutionDto = createCourseExecution(COURSE_EXECUTION_NAME, COURSE_EXECUTION_TYPE, COURSE_EXECUTION_ACRONYM, COURSE_EXECUTION_ACADEMIC_TERM, TIME_4)
+        userDto = createUser(USER_NAME_1, USER_USERNAME_1, STUDENT_ROLE)
+    }
+
+    def cleanup() {
+        behaviourService.cleanUpCounter()
+        behaviourService.cleanDirectory()
+    }
+
+    def "student is not enrolled when enrollStudentStep fails"() {
+        when: 'addStudent fails on enrollStudentStep'
+        courseExecutionFunctionalities.addStudent(courseExecutionDto.getAggregateId(), userDto.getAggregateId())
+
+        then: 'the injected fault is thrown'
+        thrown(SimulatorException)
+
+        and: 'the course execution has no students'
+        def result = courseExecutionFunctionalities.getCourseExecutionByAggregateId(courseExecutionDto.getAggregateId())
+        result.students.isEmpty()
+    }
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfigurationSagas {}
+}
