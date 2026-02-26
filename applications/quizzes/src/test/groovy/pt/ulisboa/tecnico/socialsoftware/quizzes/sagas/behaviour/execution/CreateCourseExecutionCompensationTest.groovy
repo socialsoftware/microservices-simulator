b4e519ce67817352a44dc7b2d0de8c78be5b19aa
Francisco Leitão <kikoeiraleitao@gmail.com>
2026-02-26 16:11:28 +0000

    test(Execution): add enrollStudent tests

--- applications/quizzes/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/quizzes/sagas/behaviour/execution/CreateCourseExecutionCompensationTest.groovy
+++ applications/quizzes/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/quizzes/sagas/behaviour/execution/CreateCourseExecutionCompensationTest.groovy
@@ -1,4 +1,4 @@
-package pt.ulisboa.tecnico.socialsoftware.quizzes.sagas.behaviour
+package pt.ulisboa.tecnico.socialsoftware.quizzes.sagas.behaviour.execution
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
