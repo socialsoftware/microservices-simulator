b61182abd253eaba161da487e5ed43a94743ab94
Daniel Pereira <72202548+DaniPalma2002@users.noreply.github.com>
2026-03-03 14:54:40 +0000

    Remove deprecated async and sagaAsync step classes

--- simulator/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/ms/sagas/workflow/PlanOrderTest.groovy
+++ simulator/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/ms/sagas/workflow/PlanOrderTest.groovy
@@ -6,9 +6,6 @@ import pt.ulisboa.tecnico.socialsoftware.ms.coordination.workflow.WorkflowFuncti
 import pt.ulisboa.tecnico.socialsoftware.ms.sagas.unitOfWork.SagaUnitOfWork
 import pt.ulisboa.tecnico.socialsoftware.ms.sagas.unitOfWork.SagaUnitOfWorkService
 
-import java.util.concurrent.CompletableFuture
-import java.util.concurrent.TimeUnit
-
 class PlanOrderTest extends SpockTest {
 
     def unitOfWorkService = Mock(SagaUnitOfWorkService)
@@ -19,14 +16,6 @@ class PlanOrderTest extends SpockTest {
     def step2 = new SagaStep("step2", { System.out.println("Step 2 executed") })
     def step3 = new SagaStep("step3", { System.out.println("Step 3 executed") })
     def step4 = new SagaStep("step4", { System.out.println("Step 4 executed") })
-    def asyncStep1 = new SagaAsyncStep("SagaAsyncStep1", () -> CompletableFuture.runAsync(() -> {
-        try { TimeUnit.MILLISECONDS.sleep(200); } catch (InterruptedException e) { e.printStackTrace(); }
-        System.out.println("Async Step 1 executed");
-    }))
-    def asyncStep2 = new SagaAsyncStep("SagaAsyncStep2", () -> CompletableFuture.runAsync(() -> {
-        try { TimeUnit.MILLISECONDS.sleep(200); } catch (InterruptedException e) { e.printStackTrace(); }
-        System.out.println("Async Step 2 executed");
-    }))
 
     def "test planOrder with no dependencies"() {
         given:
@@ -90,75 +79,5 @@ class PlanOrderTest extends SpockTest {
 
         then:
         thrown(IllegalStateException)
-    }
-    
-    def "test planOrder with async steps and no dependencies"() {
-        given:
-        def stepsWithNoDependencies = new HashMap<FlowStep, ArrayList<FlowStep>>()
-
-        stepsWithNoDependencies.put(asyncStep1, new ArrayList<>())
-        stepsWithNoDependencies.put(asyncStep2, new ArrayList<>())
-
-        when:
-        def executionPlan = workflow.planOrder(stepsWithNoDependencies)
-
-        then:
-        executionPlan.getPlan().size() == 2
-        executionPlan.getPlan().contains(asyncStep1)
-        executionPlan.getPlan().contains(asyncStep2)
-    }
-
-    def "test planOrder with mixed sync and async steps and dependencies"() {
-        given:
-        def stepsWithMixedDependencies = new HashMap<FlowStep, ArrayList<FlowStep>>()
-
-        stepsWithMixedDependencies.put(step1, new ArrayList<>())
-        stepsWithMixedDependencies.put(asyncStep1, new ArrayList<>([step1]))
-        stepsWithMixedDependencies.put(step2, new ArrayList<>([asyncStep1]))
-        stepsWithMixedDependencies.put(asyncStep2, new ArrayList<>([step2]))
-
-        when:
-        def executionPlan = workflow.planOrder(stepsWithMixedDependencies)
-
-        then:
-        executionPlan.getPlan() == [step1, asyncStep1, step2, asyncStep2]
-    }
-
-    def "test planOrder with mixed sync and async steps and complex dependencies"() {
-        given:
-        def stepsWithComplexMixedDependencies = new HashMap<FlowStep, ArrayList<FlowStep>>()
-
-        stepsWithComplexMixedDependencies.put(step1, new ArrayList<>())
-        stepsWithComplexMixedDependencies.put(asyncStep1, new ArrayList<>([step1]))
-        stepsWithComplexMixedDependencies.put(step2, new ArrayList<>([asyncStep1]))
-        stepsWithComplexMixedDependencies.put(step3, new ArrayList<>([asyncStep1]))
-        stepsWithComplexMixedDependencies.put(asyncStep2, new ArrayList<>([step2, step3]))
-        stepsWithComplexMixedDependencies.put(step4, new ArrayList<>([asyncStep2]))
-
-        when:
-        def executionPlan = workflow.planOrder(stepsWithComplexMixedDependencies)
-
-        then:
-        def validPlans = [
-            [step1, asyncStep1, step2, step3, asyncStep2, step4],
-            [step1, asyncStep1, step3, step2, asyncStep2, step4]
-        ]
-        validPlans.contains(executionPlan.getPlan())
-    }
-
-    def "test async steps run asynchronously"() {
-        given:
-        def stepsWithNoDependencies = new HashMap<FlowStep, ArrayList<FlowStep>>()
-        stepsWithNoDependencies.put(asyncStep1, new ArrayList<>())
-        stepsWithNoDependencies.put(asyncStep2, new ArrayList<>())
-
-        when:
-        def start = System.currentTimeMillis()
-        workflow.planOrder(stepsWithNoDependencies).execute().join()
-        def end = System.currentTimeMillis()
-        def duration = end - start
-
-        then:
-        duration < 400 // Check if async steps are running in parallel
     }
 }
