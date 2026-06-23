36c6a982406a49b155aaecd8a4f9fb69a35e35db
kikoleitao <kikoeiraleitao@gmail.com>
2026-06-23 22:43:39 +0100

    feat: promote UpdateUserName/AnonymizeUser from T2 to T3 functionality tests on quizzes-full User

--- applications/quizzes-full/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/quizzesfull/sagas/coordination/user/AnonymizeUserTest.groovy
+++ applications/quizzes-full/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/quizzesfull/sagas/coordination/user/AnonymizeUserTest.groovy
@@ -1,12 +1,17 @@
 package pt.ulisboa.tecnico.socialsoftware.quizzesfull.sagas.coordination.user
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Import
 import org.springframework.transaction.annotation.Transactional
+import pt.ulisboa.tecnico.socialsoftware.ms.messaging.CommandGateway
+import pt.ulisboa.tecnico.socialsoftware.ms.transaction.sagas.aggregate.GenericSagaState
 import pt.ulisboa.tecnico.socialsoftware.quizzesfull.BeanConfigurationSagas
 import pt.ulisboa.tecnico.socialsoftware.quizzesfull.QuizzesFullSpockTest
 import pt.ulisboa.tecnico.socialsoftware.quizzesfull.microservices.user.aggregate.UserDto
+import pt.ulisboa.tecnico.socialsoftware.quizzesfull.microservices.user.aggregate.sagas.states.UserSagaState
+import pt.ulisboa.tecnico.socialsoftware.quizzesfull.microservices.user.coordination.sagas.AnonymizeUserFunctionalitySagas
 
 @DataJpaTest
 @Transactional
@@ -16,6 +21,9 @@ class AnonymizeUserTest extends QuizzesFullSpockTest {
     @TestConfiguration
     static class LocalBeanConfiguration extends BeanConfigurationSagas {}
 
+    @Autowired
+    CommandGateway commandGateway
+
     UserDto userDto
 
     def setup() {
@@ -23,14 +31,36 @@ class AnonymizeUserTest extends QuizzesFullSpockTest {
     }
 
     def "anonymizeUser: success — name and username set to ANONYMOUS"() {
+        // Spec: User.{name,username} = "ANONYMOUS"; SagaState after commit == NOT_IN_SAGA.
+        // Source: UserService.anonymizeUser.
         when:
-        def uow = unitOfWorkService.createUnitOfWork("anonymizeUser")
-        userService.anonymizeUser(userDto.aggregateId, uow)
-        unitOfWorkService.commit(uow)
+        userFunctionalities.anonymizeUser(userDto.aggregateId)
 
         then:
-        def readUow = unitOfWorkService.createUnitOfWork("check")
-        def result = userService.getUserById(userDto.aggregateId, readUow)
+        def result = userFunctionalities.getUserById(userDto.aggregateId)
+        result.name == "ANONYMOUS"
+        result.username == "ANONYMOUS"
+        sagaStateOf(userDto.aggregateId) == GenericSagaState.NOT_IN_SAGA
+    }
+
+    def "anonymizeUser: getUserStep acquires READ_USER semantic lock"() {
+        given: 'an anonymizeUser workflow pauses after getUserStep has acquired READ_USER lock'
+        def uow = unitOfWorkService.createUnitOfWork("anonymizeUser")
+        def func = new AnonymizeUserFunctionalitySagas(
+                unitOfWorkService, userDto.aggregateId, uow, commandGateway)
+        func.executeUntilStep("getUserStep", uow)
+
+        expect: 'user saga state is READ_USER'
+        sagaStateOf(userDto.aggregateId) == UserSagaState.READ_USER
+
+        when: 'workflow resumes and completes'
+        func.resumeWorkflow(uow)
+
+        then:
+        noExceptionThrown()
+
+        and: 'name and username were anonymized'
+        def result = userFunctionalities.getUserById(userDto.aggregateId)
         result.name == "ANONYMOUS"
         result.username == "ANONYMOUS"
     }
--- applications/quizzes-full/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/quizzesfull/sagas/coordination/user/UpdateUserNameTest.groovy
+++ applications/quizzes-full/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/quizzesfull/sagas/coordination/user/UpdateUserNameTest.groovy
@@ -1,12 +1,17 @@
 package pt.ulisboa.tecnico.socialsoftware.quizzesfull.sagas.coordination.user
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Import
 import org.springframework.transaction.annotation.Transactional
+import pt.ulisboa.tecnico.socialsoftware.ms.messaging.CommandGateway
+import pt.ulisboa.tecnico.socialsoftware.ms.transaction.sagas.aggregate.GenericSagaState
 import pt.ulisboa.tecnico.socialsoftware.quizzesfull.BeanConfigurationSagas
 import pt.ulisboa.tecnico.socialsoftware.quizzesfull.QuizzesFullSpockTest
 import pt.ulisboa.tecnico.socialsoftware.quizzesfull.microservices.user.aggregate.UserDto
+import pt.ulisboa.tecnico.socialsoftware.quizzesfull.microservices.user.aggregate.sagas.states.UserSagaState
+import pt.ulisboa.tecnico.socialsoftware.quizzesfull.microservices.user.coordination.sagas.UpdateUserNameFunctionalitySagas
 
 @DataJpaTest
 @Transactional
@@ -16,6 +21,9 @@ class UpdateUserNameTest extends QuizzesFullSpockTest {
     @TestConfiguration
     static class LocalBeanConfiguration extends BeanConfigurationSagas {}
 
+    @Autowired
+    CommandGateway commandGateway
+
     UserDto userDto
 
     def setup() {
@@ -23,14 +31,34 @@ class UpdateUserNameTest extends QuizzesFullSpockTest {
     }
 
     def "updateUserName: success"() {
+        // Spec: User.name = newName; SagaState after commit == NOT_IN_SAGA.
+        // Source: UserService.updateUserName — only field this functionality mutates is name.
         when:
-        def uow = unitOfWorkService.createUnitOfWork("updateUserName")
-        userService.updateUserName(userDto.aggregateId, USER_NAME_2, uow)
-        unitOfWorkService.commit(uow)
+        userFunctionalities.updateUserName(userDto.aggregateId, USER_NAME_2)
 
         then:
-        def readUow = unitOfWorkService.createUnitOfWork("check")
-        def result = userService.getUserById(userDto.aggregateId, readUow)
+        def result = userFunctionalities.getUserById(userDto.aggregateId)
         result.name == USER_NAME_2
+        sagaStateOf(userDto.aggregateId) == GenericSagaState.NOT_IN_SAGA
+    }
+
+    def "updateUserName: getUserStep acquires READ_USER semantic lock"() {
+        given: 'an updateUserName workflow pauses after getUserStep has acquired READ_USER lock'
+        def uow = unitOfWorkService.createUnitOfWork("updateUserName")
+        def func = new UpdateUserNameFunctionalitySagas(
+                unitOfWorkService, userDto.aggregateId, USER_NAME_2, uow, commandGateway)
+        func.executeUntilStep("getUserStep", uow)
+
+        expect: 'user saga state is READ_USER'
+        sagaStateOf(userDto.aggregateId) == UserSagaState.READ_USER
+
+        when: 'workflow resumes and completes'
+        func.resumeWorkflow(uow)
+
+        then:
+        noExceptionThrown()
+
+        and: 'name was updated'
+        userFunctionalities.getUserById(userDto.aggregateId).name == USER_NAME_2
     }
 }
