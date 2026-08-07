d5fa181ab0758f7f6c8af3f0c3f3fd2d72ef257e
Martim Mendes <martimccmendes@tecnico.ulisboa.pt>
2026-08-07 13:07:05 +0100

    feat(rl-server): Redesign docker compose and startup scripts to only use the bare minimum containers for training. Also created a debug docker profile to enable Jaeger for testing.

--- performance-analysis/start_parallel.sh
+++ performance-analysis/start_parallel.sh
@@ -2,6 +2,8 @@
 cd "$(dirname "$0")"
 
 N=${1:-1}
+PROFILE=${2:-train} # Defaults to 'train' if not provided
+
 for ((i=1; i<=N; i++))
 do
     export VERSION_MODE="distributed-version"
@@ -9,6 +11,7 @@ do
     export H2_PORT=$((1521+i))
     export GRPC_PORT=$((4319+i))
     export JAEGER_PORT=$((16686+i))
-    echo "Starting worker_$i on Gateway Port $GATEWAY_PORT, H2 Port $H2_PORT, gRPC Port $GRPC_PORT, Jaeger Port $JAEGER_PORT"
-    docker compose --project-directory .. -p worker_$i -f docker-compose-parallel-training.yml up -d quizzes-rl
+    echo "Starting worker_$i on Gateway Port $GATEWAY_PORT, H2 Port $H2_PORT, gRPC Port $GRPC_PORT, Jaeger Port $JAEGER_PORT with profile: $PROFILE"
+    docker compose --project-directory .. -p worker_$i -f docker-compose-parallel-training.yml --profile $PROFILE up -d quizzes-$PROFILE
+    sleep 5
 done
--- performance-analysis/stop_parallel.sh
+++ performance-analysis/stop_parallel.sh
@@ -2,8 +2,8 @@
 cd "$(dirname "$0")"
 
 N=${1:-1}
-for ((i=0; i<=N; i++))
+for ((i=1; i<=N; i++))
 do
     echo "Stopping worker_$i"
-    docker compose --project-directory .. -p worker_$i -f docker-compose-parallel-training.yml down -v --remove-orphans
+    docker compose --project-directory .. -p worker_$i -f docker-compose-parallel-training.yml --profile train --profile debug down -v --remove-orphans
 done
