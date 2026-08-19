c71d7f94af09deaee7c6d659b4e5af72bafc9196
Martim Mendes <martimccmendes@tecnico.ulisboa.pt>
2026-08-19 17:15:15 +0100

    feat(rl-server): Added a few fail-safes to docker to not crash processes. Optimized the start script so it only builds the image once. Updates to training configurations.

--- performance-analysis/start_parallel.sh
+++ performance-analysis/start_parallel.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -e
 cd "$(dirname "$0")"
 
 N=${1:-1}
 PROFILE=${2:-train} # Defaults to 'train' if not provided
+
+docker compose --project-directory .. -f docker-compose-parallel-training.yml --profile "$PROFILE" build quizzes-"$PROFILE"
 
 for ((i=1; i<=N; i++))
 do
@@ -11,7 +14,7 @@ do
     export H2_PORT=$((1521+i))
     export GRPC_PORT=$((4319+i))
     export JAEGER_PORT=$((16686+i))
-    echo "Starting worker_$i on Gateway Port $GATEWAY_PORT, H2 Port $H2_PORT, gRPC Port $GRPC_PORT, Jaeger Port $JAEGER_PORT with profile: $PROFILE"
-    docker compose --project-directory .. -p worker_$i -f docker-compose-parallel-training.yml --profile $PROFILE up -d quizzes-$PROFILE
+    echo "Starting martim03_$i on Gateway Port $GATEWAY_PORT, H2 Port $H2_PORT, gRPC Port $GRPC_PORT, Jaeger Port $JAEGER_PORT with profile: $PROFILE"
+    docker compose --project-directory .. -p martim03_$i -f docker-compose-parallel-training.yml --profile $PROFILE up -d quizzes-$PROFILE
     sleep 5
 done
--- performance-analysis/stop_parallel.sh
+++ performance-analysis/stop_parallel.sh
@@ -4,6 +4,7 @@ cd "$(dirname "$0")"
 N=${1:-1}
 for ((i=1; i<=N; i++))
 do
-    echo "Stopping worker_$i"
-    docker compose --project-directory .. -p worker_$i -f docker-compose-parallel-training.yml --profile train --profile debug down -v --remove-orphans
+    echo "Stopping martim03_$i"
+    docker compose --project-directory .. -p martim03_$i -f docker-compose-parallel-training.yml --profile train --profile debug down -t 1 -v --remove-orphans
+    sleep 0.5
 done
